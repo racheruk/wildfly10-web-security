fdd95c2bf91d62e8b1bdbc381dc2ba0b62e9e6b5
Rakesh K. Cherukuri <racheruk@gmail.com>
2016-04-16 21:54:53 +0530

    modified code to be consice

--- basic-db-authentication/src/main/java/com/rakesh/security/basic/db/authentication/util/Hash.java
+++ basic-db-authentication/src/main/java/com/rakesh/security/basic/db/authentication/util/Hash.java
@@ -10,20 +10,16 @@ import java.security.NoSuchAlgorithmException;
 public class Hash {
 
   public static String createWildFlyPasswordHash(String userName, String password) {
-    String output = null;
     try {
       String str = String.join("", userName, ":ApplicationRealm:", password);
 
       MessageDigest digest = MessageDigest.getInstance("MD5");
 
-      output = String.format("%x", new BigInteger(1, digest.digest(str.getBytes())));
-
-      System.out.println("Message hex(md5()) = " + output);
+      return String.format("%x", new BigInteger(1, digest.digest(str.getBytes())));
     } catch (Exception e) {
       e.printStackTrace();
     }
-
-    return output;
+    return null;
   }
 
 }
